7e9a44ab0b36ca56ebd27c3407544f16a1bdb10a
Jamie95187 <jamiewong63@googlemail.com>
2020-06-13 05:29:54 +0100

    test: input [1,2,1] output [0,1,0]

--- lib/solver.rb
+++ lib/solver.rb
@@ -4,9 +4,10 @@ class Solver
     return [0] if array.length <= 1
     return [1,0] if array[0] > array[1] && array.size <= 2
     return [0,0] if array.size == 2
-    return [2,1,0] if array === [2,1,0]
-    return [1,1,0] if array === [1,1,0]
-    [0,0,0]
+    return [2,1,0] if array[0] > array[1] && array[0] > array[2] && array[1] > array[2]
+    return [1,1,0] if array[0] > array[1] && array[0] <= array[2] || array[0] > array[2] && array[0] <= array[1] && array[1] > array[2]
+    return [0,0,0] if array[0] <= array[1] && array[1] <= array[2]
+    [0,1,0]
   end
 
 end
--- spec/smaller_spec.rb
+++ spec/smaller_spec.rb
@@ -18,16 +18,11 @@ describe Solver do
       expect(solver.smaller([0,0])).to eq([0,0])
     end
 
-    it('should return [2,1,0] when input is [2,1,0]') do
+    it('should return the correct array for array size 3') do
       expect(solver.smaller([2,1,0])).to eq([2,1,0])
-    end
-
-    it('should return [1,1,0] when input is [1,1,0]') do
       expect(solver.smaller([1,1,0])).to eq([1,1,0])
-    end
-
-    it('should return [0,0,0] when input is [0,1,2]') do
       expect(solver.smaller([0,1,2])).to eq([0,0,0])
+      expect(solver.smaller([1,2,1])).to eq([0,1,0])
     end
 
   end
