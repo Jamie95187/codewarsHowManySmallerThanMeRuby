c231fc5f6228f756c25d46ffc8b520503cb187d9
Jamie95187 <jamiewong63@googlemail.com>
2020-06-13 04:52:49 +0100

    test: input [0,0] output [0,0]

--- lib/solver.rb
+++ lib/solver.rb
@@ -3,7 +3,7 @@ class Solver
   def smaller(array)
     return [0] if array.length <= 1
     return [1,0] if array == [1,0]
-    return [0,0] if array == [0,1]
+    return [0,0] if array == [0,1] || array == [0,0]
     [2,1,0]
   end
 
--- spec/smaller_spec.rb
+++ spec/smaller_spec.rb
@@ -11,12 +11,10 @@ describe Solver do
       expect(solver.smaller([2])).to eq([0])
     end
 
-    it('should return [1,0] when input is [1,0]') do
+    it('should return correct array for input size 2') do
       expect(solver.smaller([1,0])).to eq([1,0])
-    end
-
-    it('should return [0,0] when input is [0,1]') do
       expect(solver.smaller([0,1])).to eq([0,0])
+      expect(solver.smaller([0,0])).to eq([0,0])
     end
 
     it('should return [2,1,0] when input is [2,1,0]') do
