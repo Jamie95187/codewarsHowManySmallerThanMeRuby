2e0582afe5ca04249bd3fd692e708a450f27fbd5
Jamie95187 <jamiewong63@googlemail.com>
2020-06-14 23:24:17 +0100

    failing codewars efficiency test

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,6 +1,23 @@
 class Solver
 
   def smaller(array)
+    return [0] if array.length == 1
+    count = []
+    save = {}
+    k = array.length - 1
+    k =- 1
+    save[array[k]] = 0
+    count << save[array[k]]
+    while k >= 0 do
+      count.each { | | }
+    end
+    arrayMax = array
+    arrayMax[array.length] = array.last
+    currentMax = array.last
+    i = array.length - 2
+    while i >= 0 do
+      currentMax = array[i] if array[i] > currentMax
+    end
     answer = []
     i = 0
     while i < array.length do
